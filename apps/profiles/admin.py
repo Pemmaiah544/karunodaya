9f82a120d62b90e216426850daf21896022f2a5a
Pemmaiah544 <pemmaiah@navadhiti.com>
2026-01-27 17:19:43 +0530

    implemented search functionality

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,6 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.models import User
 from unfold.admin import ModelAdmin, TabularInline
 from .models import ParentProfile, Child
+
+
+# Custom User Admin to override the default Django User admin
+admin.site.unregister(User)
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    list_display = ('get_phone_number', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'groups', 'date_joined')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'parent_profile__phone_number')
+    ordering = ('username',)
+    
+    def get_phone_number(self, obj):
+        try:
+            return obj.parent_profile.phone_number
+        except ParentProfile.DoesNotExist:
+            return 'N/A'
+    get_phone_number.short_description = 'Phone Number'
+    
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined'),
+        }),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
 
 
 class ChildInline(TabularInline):
@@ -12,11 +53,16 @@ class ChildInline(TabularInline):
 
 @admin.register(ParentProfile)
 class ParentProfileAdmin(ModelAdmin):
-    list_display = ('user', 'phone_number', 'city', 'children_count', 'created_at')
-    list_filter = ('city', 'state', 'created_at')
-    search_fields = ('user__username', 'user__email', 'phone_number', 'city')
+    list_display = ('user', 'phone_number', 'user__email', 'user__first_name', 'user__last_name', 'is_staff_status', 'city', 'children_count', 'created_at')
+    list_filter = ('city', 'state', 'created_at', 'user__is_staff')
+    search_fields = ('user__first_name', 'user__last_name', 'user__email', 'phone_number')
     readonly_fields = ('created_at', 'updated_at')
     inlines = [ChildInline]
+
+    def is_staff_status(self, obj):
+        return obj.user.is_staff
+    is_staff_status.boolean = True
+    is_staff_status.short_description = 'Staff Status'
 
     fieldsets = (
         ('User Information', {
