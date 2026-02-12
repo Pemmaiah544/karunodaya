5306e8af188af2bbb168b1bd172e1668edf9cb42
Pemmaiah544 <pemmaiah@navadhiti.com>
2026-02-12 15:15:05 +0530

    Merge develop and fix CSS conflict

--- karunodaya_project/settings.py
+++ karunodaya_project/settings.py
@@ -113,10 +113,17 @@ WSGI_APPLICATION = 'karunodaya_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='karunodaya'),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
